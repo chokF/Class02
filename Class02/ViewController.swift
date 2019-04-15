f817c8139bbf7390990a5a35542be096c9e9d3b6
L20102-02 <liao@mail.cjcu.edu.tw>
2019-04-15 11:23:04 +0800

    edit

--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -13,12 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let a : Int = 0
+        
+        if(a > 0){
+            print(">0")
+        }else{
+            print("<=0")
+        }
+        
     }
     
-    //
-    /*
-     hi
-    */
-
+    func add(a: Int,b:Int) -> Int {
+        return a+b
+    }
+   
+    
 }
 
