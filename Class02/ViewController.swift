c11e7e831262ad4c7e057a70b35e2e4fdaed1e1c
L20102-02 <liao@mail.cjcu.edu.tw>
2019-04-15 10:23:07 +0800

    edit

--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //
+    /*
+     hi
+    */
 
 }
 
